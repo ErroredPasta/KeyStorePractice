6bc2718a72a8d946ae4d4b6013bdc8cf4c416c7e
ErroredPasta <ForeignServices3132@pm.me>
2024-05-09 03:11:52 +0900

    feature: use encrypted shared preference instead of shared preference

--- app/src/main/java/com/example/keystorepractice/data/repository/SharedPreferenceRepository.kt
+++ app/src/main/java/com/example/keystorepractice/data/repository/SharedPreferenceRepository.kt
@@ -1,11 +1,13 @@
 package com.example.keystorepractice.data.repository
 
 import android.content.SharedPreferences
+import com.example.keystorepractice.di.Preference
+import com.example.keystorepractice.di.PreferenceType
 import com.example.keystorepractice.domain.PreferenceRepository
 import javax.inject.Inject
 
 class SharedPreferenceRepository @Inject constructor(
-    private val sharedPreference: SharedPreferences
+    @Preference(PreferenceType.ENCRYPTED) private val sharedPreference: SharedPreferences
 ) : PreferenceRepository {
     override suspend fun saveToken(token: String) = with(sharedPreference.edit()) {
         putString("token", token)
--- app/src/main/java/com/example/keystorepractice/di/PreferenceModule.kt
+++ app/src/main/java/com/example/keystorepractice/di/PreferenceModule.kt
@@ -2,6 +2,9 @@ package com.example.keystorepractice.di
 
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences.PrefKeyEncryptionScheme
+import androidx.security.crypto.MasterKeys
 import com.example.keystorepractice.data.repository.SharedPreferenceRepository
 import com.example.keystorepractice.domain.PreferenceRepository
 import dagger.Binds
@@ -10,6 +13,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module
@@ -21,7 +25,36 @@ interface PreferenceModule {
     companion object {
         @Provides
         @Singleton
+        @Preference(PreferenceType.PLAIN)
         fun provideSharedPreference(@ApplicationContext context: Context): SharedPreferences =
             context.getSharedPreferences("shared_preference", Context.MODE_PRIVATE)
+
+        @Provides
+        @MasterKey
+        fun provideMasterKey() = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+
+        @Provides
+        @Singleton
+        @Preference(PreferenceType.ENCRYPTED)
+        fun provideEncryptedSharedPreference(
+            @MasterKey masterKey: String,
+            @ApplicationContext context: Context,
+        ) = EncryptedSharedPreferences.create(
+            "encrypted_shared_preference",
+            masterKey,
+            context,
+            PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
     }
 }
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class MasterKey
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Preference(val type: PreferenceType)
+
+enum class PreferenceType { PLAIN, ENCRYPTED }
